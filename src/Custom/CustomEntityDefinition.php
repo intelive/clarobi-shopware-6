b34ddd60fa1e371d788de1ad7e58b910d7940e3a
Georgiana G <gitan_georgiana@yahoo.com>
2020-04-08 17:27:21 +0300

    custom entity added for testing

---
+++ src/Custom/CustomEntityDefinition.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Clarobi\Custom;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+
+class CustomEntityDefinition extends EntityDefinition
+{
+    public function getEntityName(): string
+    {
+        return 'custom_entity';
+    }
+
+    public function getCollectionClass(): string
+    {
+        return CustomEntityCollection::class;
+    }
+
+    public function getEntityClass(): string
+    {
+        return CustomEntity::class;
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
+            new StringField('technical_name', 'technicalName'),
+        ]);
+    }
+}
