002e186271438e20bdb629cfbaee89b708524745
Georgiana G <gitan_georgiana@yahoo.com>
2020-04-22 12:38:06 +0300

    Document entity extension - not working

---
+++ src/Struct/DocumentStruct.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Clarobi\Struct;
+
+use Shopware\Core\Framework\Struct\Struct;
+
+class DocumentStruct extends Struct
+{
+    public $clarobiAutoIncrement;
+}
