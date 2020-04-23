ef0e9c1b2a7f5c9b43437cb8fb299e20f2696f57
Georgiana G <gitan_georgiana@yahoo.com>
2020-04-23 16:25:59 +0300

    document controller

--- src/Core/Api/ClarobiDocumentsController.php
+++ src/Core/Api/ClarobiDocumentsController.php
@@ -2,20 +2,20 @@
 
 namespace Clarobi\Core\Api;
 
-use Clarobi\Core\Framework\Controller\ClarobiAbstractController;
-use Clarobi\Service\EncodeResponseService;
-use Clarobi\Service\ClarobiConfigService;
+use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Clarobi\Service\ClarobiConfigService;
+use Clarobi\Service\EncodeResponseService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Shopware\Core\Checkout\Document\DocumentEntity;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Clarobi\Core\Framework\Controller\ClarobiAbstractController;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\RangeFilter;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class ClarobiDocumentsController
@@ -44,13 +44,33 @@ class ClarobiDocumentsController extends ClarobiAbstractController
      */
     protected $encodeResponse;
 
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    protected $hexIds = [];
+    protected $incrementIds = [];
+
+    const ENTITY_NAME = 'document';
+
+    const IGNORED_KEYS = [
+//        'id', 'order', 'config', 'sent', 'static', 'documentType', 'createdAt', 'updatedAt',
+//        'fileType',
+        'orderId', 'orderVersionId', 'documentTypeId', 'documentMediaFileId', 'deepLinkCode', 'customFields',
+        'referencedDocumentId', 'referencedDocument', 'dependentDocuments', 'documentMediaFile',
+        '_uniqueIdentifier', 'versionId', 'translated', 'extensions',
+    ];
+
     public function __construct(
+        Connection $connection,
         EntityRepositoryInterface $documentRepository,
         EntityRepositoryInterface $documentTypeRepository,
         ClarobiConfigService $configService,
         EncodeResponseService $responseService
     )
     {
+        $this->connection = $connection;
         $this->documentRepository = $documentRepository;
         $this->documentTypeRepository = $documentTypeRepository;
         $this->config = $configService;
@@ -59,8 +79,10 @@ class ClarobiDocumentsController extends ClarobiAbstractController
 
     /**
      * @Route("/clarobi/documents", name="clarobi.documents.list", methods={"GET"})
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function listAction(Request $request)
+    public function listAction(Request $request): JsonResponse
     {
         /**
          * @todo filter documents by type in claro_app
@@ -68,30 +90,95 @@ class ClarobiDocumentsController extends ClarobiAbstractController
 
         try {
             // Verify request
-            $this->verifyRequest($request, $this->config->getConfigs());
+            $this->verifyParam($request);
+            $this->verifyToken($request, $this->config->getConfigs());
             // Get param
             $from_id = $request->get('from_id');
 
-            $from_id = $this->decToHex($from_id);
+            $selectIdAutoInc = $this->connection->executeQuery('
+                    SELECT `id`, `clarobi_auto_increment`
+                    FROM `document`
+                    WHERE `clarobi_auto_increment` > ' . $from_id . '
+                    ORDER BY `clarobi_auto_increment` ASC LIMIT 50 ;
+            ');
+            $idsResult = $selectIdAutoInc->fetchAll();
+
+            /**
+             * Get array with ids in hex to use them in search
+             * Get array with auto increment ids to used them for from_id param
+             */
+            if ($idsResult) {
+                foreach ($idsResult as $item) {
+                    $this->hexIds[] = Uuid::fromBytesToHex($item['id']);
+                    $this->incrementIds[Uuid::fromBytesToHex($item['id'])] = $item['clarobi_auto_increment'];
+                }
+            }
+            $context = Context::createDefaultContext();
+            $criteria = new Criteria($this->hexIds);
+            $criteria->addAssociation('order');
+
+            /** @var EntityCollection $entities */
+            $entities = $this->documentRepository->search($criteria, $context);
+
+            $mappedEntities = [];
+            /** @var DocumentEntity $element */
+            foreach ($entities->getElements() as $element) {
+                $mappedEntities[] = $this->mapDocumentEntity($element->jsonSerialize());
+            }
+            $lastId = $this->incrementIds[$element->getId()];
+
+            return new JsonResponse($this->encodeResponse->encodeResponse(
+                $mappedEntities,
+                self::ENTITY_NAME,
+                $lastId
+            ));
         } catch (\Exception $exception) {
             return new JsonResponse(['status' => 'error', 'message' => $exception->getMessage()]);
         }
 
-        $context = Context::createDefaultContext();
-        $criteria = new Criteria();
-        $criteria->setLimit(10)
-            ->addFilter(new RangeFilter('id', ['gte' => $from_id]))
-            ->addSorting(new FieldSorting('id', FieldSorting::ASCENDING));
+//        $resultStatement = $this->connection->executeQuery('
+//                SELECT * FROM `document`
+//                INNER JOIN `order` ON `order`.id = `document`.order_id
+//                ORDER BY `document`.`clarobi_auto_increment` ASC LIMIT 10;
+//            ');
+//        $results = $resultStatement->fetchAll();
+//        if ($results) {
+//            /** @var DocumentEntity $result */
+//            foreach ($results as $result) {
+//                var_dump(($result));
+//                die;
+//            }
+//        }
+//        $context = Context::createDefaultContext();
+//        $criteria = new Criteria();
+//        $criteria->setLimit(10)
+//            ->addFilter(new RangeFilter('id', ['gte' => $from_id]))
+//            ->addSorting(new FieldSorting('id', FieldSorting::ASCENDING))
+//            ->addAssociation('order');
+//
+//        /** @var EntityCollection $entities */
+//        $entities = $this->documentRepository->search($criteria, $context);
 
-        /** @var EntityCollection $entities */
-        $entities = $this->documentRepository->search($criteria, $context);
+    }
 
-        $mappedEntities = [];
-        foreach ($entities->getElements() as $element) {
-            $mappedEntities[] = ['id' => $this->hexToDec($element->getId())];
+    /**
+     * @param $document
+     * @return array
+     */
+    private function mapDocumentEntity($document)
+    {
+        $mappedKeys = [];
+        $mappedKeys['entity_name'] = self::ENTITY_NAME;
+        $mappedKeys['clarobi_auto_increment'] = $this->incrementIds[$document['id']];
+//            ($this->incrementIds[$document['id']] ? $this->incrementIds[$document['id']] : 0);
+
+        foreach ($document as $key => $value) {
+            if (in_array($key, self::IGNORED_KEYS)) {
+                continue;
+            }
+            $mappedKeys[$key] = $value;
         }
 
-        return new JsonResponse($mappedEntities, Response::HTTP_OK);
-//        return new JsonResponse($this->encodeResponse->encodeResponse($mappedEntities, self::ENTITY_NAME));
+        return $mappedKeys;
     }
 }
