ed53947523e03a478fa1b1da1caac68a5de97ee6
Georgiana G <gitan_georgiana@yahoo.com>
2020-04-23 14:55:37 +0300

    order controller

--- src/Core/Api/ClarobiOrderController.php
+++ src/Core/Api/ClarobiOrderController.php
@@ -2,21 +2,20 @@
 
 namespace Clarobi\Core\Api;
 
+use Shopware\Core\Framework\Context;
 use Clarobi\Service\ClarobiConfigService;
 use Clarobi\Service\EncodeResponseService;
-use Shopware\Core\Checkout\Order\OrderCollection;
 use Shopware\Core\Checkout\Order\OrderEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\Checkout\Order\OrderCollection;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Clarobi\Core\Framework\Controller\ClarobiAbstractController;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\RangeFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Sorting\FieldSorting;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Shopware\Core\Framework\Context;
 
 /**
  * Class ClarobiOrderController
@@ -24,7 +23,7 @@ use Shopware\Core\Framework\Context;
  * @RouteScope(scopes={"storefront"})
  * @package Clarobi\Core\Api
  */
-class ClarobiOrderController extends AbstractController
+class ClarobiOrderController extends ClarobiAbstractController
 {
     /**
      * @var EntityRepositoryInterface
@@ -44,46 +43,13 @@ class ClarobiOrderController extends AbstractController
     const ENTITY_NAME = 'sales_order';
 
     const IGNORED_KEYS = [
-//        'id',
-//        'autoIncrement',
-//        'orderNumber',
-//        'currencyId',
-//        'orderDateTime',
-//        'orderDate',
-//        'price',
-//        'amountTotal',
-//        'amountNet',
-//        'orderCustomer',
-//        'currency',
-//        'addresses',
-//        'deliveries',
-//        'lineItems',
-//        'transactions',
-//        'createdAt',
-//        'updatedAt',
-        'currencyFactor',
-        'salesChannelId',
-        'billingAddressId',
-        'positionPrice',
-        'taxStatus',
-        'shippingCosts',
-        'shippingTotal',
-        'languageId',
-        'language',
-        'salesChannel',
-        'deepLinkCode',
-        'stateMachineState',
-        'stateId',
-        'customFields',
-        'documents',
-        'tags',
-        'affiliateCode',
-        'campaignCode',
-        '_uniqueIdentifier',
-        'versionId',
-        'translated',
-        'extensions',
-        'billingAddressVersionId',
+//        'id', 'autoIncrement', 'orderNumber', 'currencyId', 'orderDateTime', 'orderDate', 'price', 'amountTotal',
+//        'amountNet', 'orderCustomer', 'currency', 'addresses', 'deliveries', 'lineItems', 'transactions',
+//        'createdAt', 'updatedAt',
+        'currencyFactor', 'salesChannelId', 'billingAddressId', 'positionPrice', 'taxStatus', 'shippingCosts',
+        'shippingTotal', 'languageId', 'language', 'salesChannel', 'deepLinkCode', 'stateMachineState', 'stateId',
+        'customFields', 'documents', 'tags', 'affiliateCode', 'campaignCode', '_uniqueIdentifier', 'versionId',
+        'translated', 'extensions', 'billingAddressVersionId',
     ];
 
     public function __construct(
@@ -98,60 +64,61 @@ class ClarobiOrderController extends AbstractController
     }
 
     /**
-     * "shippingTotal": 10 - is calculated if shipping settings are set
-     *
      * @Route("/clarobi/order", name="clarobi.order.list")
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function listAction(Request $request): Response
+    public function listAction(Request $request): JsonResponse
     {
         try {
             // Verify token request
-            $this->configService->verifyRequestToken($request);
+            $this->verifyParam($request);
+            $this->verifyToken($request, $this->configService->getConfigs());
             // Get param request
             $from_id = $request->get('from_id');
-            if (is_null($from_id)) {
-                throw new \Exception('Param \'from_id\' is missing!');
+
+            $context = Context::createDefaultContext();
+            $criteria = new Criteria();
+            $criteria->setLimit(1)
+                ->addFilter(new RangeFilter('autoIncrement', ['gte' => $from_id]))
+                ->addSorting(new FieldSorting('autoIncrement', FieldSorting::ASCENDING))
+                ->addAssociation('lineItems.product.categories')
+                ->addAssociation('addresses')
+                ->addAssociation('addresses.country')
+                ->addAssociation('addresses.state')
+                ->addAssociation('deliveries')
+                ->addAssociation('transactions.paymentMethod');
+
+            /**
+             * @todo add association for discount code
+             * "shippingTotal": 10 - is calculated if shipping settings are set
+             */
+
+            /** @var OrderCollection $entities */
+            $entities = $this->orderRepository->search($criteria, $context);
+
+            $mappedEntities = [];
+            /** @var OrderEntity $element */
+            foreach ($entities->getElements() as $element) {
+                $mappedEntities[] = $this->mapOrderEntity($element->jsonSerialize());
             }
+            $lastId = $element->getAutoIncrement();
+
+            return new JsonResponse($this->encodeResponse->encodeResponse(
+                $mappedEntities,
+                self::ENTITY_NAME,
+                $lastId
+            ));
         } catch (\Exception $exception) {
             return new JsonResponse(['status' => 'error', 'message' => $exception->getMessage()]);
         }
-        /**
-         * uuid for customer,doc,orders
-         * extend cart - add uuid?
-         *
-         */
-
-        $context = Context::createDefaultContext();
-        $criteria = new Criteria();
-        $criteria->setLimit(1)
-            ->addFilter(new RangeFilter('autoIncrement', ['gte' => $from_id]))
-            ->addSorting(new FieldSorting('autoIncrement', FieldSorting::ASCENDING))
-            ->addAssociation('lineItems.product.categories')
-            ->addAssociation('addresses')
-            ->addAssociation('addresses.country')
-            ->addAssociation('addresses.state')
-            ->addAssociation('deliveries')
-            ->addAssociation('transactions.paymentMethod');
-
-//        $criteria->setIncludes(['autoIncrement', 'lineItems', 'amountTotal', 'orderCustomer']);
-
-        /**
-         * @todo add association for discount code
-         */
-
-        /** @var OrderCollection $entities */
-        $entities = $this->orderRepository->search($criteria, $context);
-
-        $mappedEntities = [];
-        /** @var OrderEntity $element */
-        foreach ($entities->getElements() as $element) {
-            $mappedEntities[] = $this->mapOrderEntity($element->jsonSerialize());
-        }
-
-        return new JsonResponse($mappedEntities);
-//        return new JsonResponse($this->encodeResponse->encodeResponse($mappedEntities, self::ENTITY_NAME));
     }
 
+    /**
+     * @param $order
+     * @return mixed
+     */
     private function mapOrderEntity($order)
     {
         $mappedKeys['entity_name'] = self::ENTITY_NAME;
@@ -164,5 +131,4 @@ class ClarobiOrderController extends AbstractController
 
         return $mappedKeys;
     }
-
 }
