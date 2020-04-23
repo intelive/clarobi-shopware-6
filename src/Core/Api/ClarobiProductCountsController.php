82d9fc8af4d3bf276664f81d50131850b77098b0
Georgiana G <gitan_georgiana@yahoo.com>
2020-04-23 14:58:39 +0300

    product counters controller

--- src/Core/Api/ClarobiProductCountsController.php
+++ src/Core/Api/ClarobiProductCountsController.php
@@ -2,15 +2,22 @@
 
 namespace Clarobi\Core\Api;
 
+use Doctrine\DBAL\Connection;
 use Clarobi\Service\ClarobiConfigService;
 use Clarobi\Service\EncodeResponseService;
-use Doctrine\DBAL\Connection;
-use Shopware\Core\Framework\Routing\Annotation\RouteScope;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Clarobi\Core\Framework\Controller\ClarobiAbstractController;
 
-class ClarobiProductCountsController extends AbstractController
+class ClarobiProductCountsController extends ClarobiAbstractController
 {
+    const ENTITY_NAME = 'product_counter';
+    const ENTITY_TYPE = 'PRODUCT_COUNTERS';
+
+    const PRODUCT_COUNTER_TABLE = 'clarobi_product_counts';
+
     /**
      * @var Connection
      */
@@ -25,6 +32,7 @@ class ClarobiProductCountsController extends AbstractController
      * @var ClarobiConfigService
      */
     protected $configService;
+    protected $configs;
 
     public function __construct(
         Connection $connection,
@@ -34,22 +42,50 @@ class ClarobiProductCountsController extends AbstractController
     {
         $this->connection = $connection;
         $this->configService = $configService;
+        $this->configs = $this->configService->getConfigs();
         $this->encodeResponse = $responseService;
     }
 
     /**
      * @RouteScope(scopes={"storefront"})
      * @Route("/clarobi/productCounters", name="clarobi.product.counters", methods={"GET"})
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function dataCountersAction()
+    public function productCountersAction(Request $request): JsonResponse
     {
-        /**
-         * @todo add subscriber for events
-         * sales_channel.product.id.search.result.loaded
-         * product.search.result.loaded - after the search returned data
-         * product.id.search.result.loaded - after the search for the ids has been finished
-         */
+        try {
+            // Verify request
+            $this->verifyToken($request, $this->configService->getConfigs());
 
-//        return new JsonResponse($this->encodeResponse->encodeResponse($mappedEntities, self::ENTITY_NAME))
+            $results = $this->connection->executeQuery("
+                SELECT * FROM " . self::PRODUCT_COUNTER_TABLE
+                . " ORDER BY `product_auto_increment` ASC;
+                ")->fetchAll();
+
+            $date = date('Y-m-d H:i:s', time());
+            $data = [
+                'date' => $date,
+                'counters' => []
+            ];
+            if ($results) {
+                foreach ($results as $result) {
+                    $data['counters'][] = [
+                        'product_id' => $result['product_auto_increment'],
+                        'event_name' => 'catalog_product_view',
+                        'viewed' => $result['views']
+                    ];
+                    $data['counters'][] = [
+                        'product_id' => $result['product_auto_increment'],
+                        'event_name' => 'catalog_cart_add_product',
+                        'viewed' => $result['adds_to_cart']
+                    ];
+                }
+            }
+            return new JsonResponse($this->encodeResponse->encodeResponse($data, self::ENTITY_NAME, self::ENTITY_TYPE));
+        } catch (\Exception $exception) {
+            return new JsonResponse(['status' => 'error', 'message' => $exception->getMessage()]);
+        }
     }
 }
